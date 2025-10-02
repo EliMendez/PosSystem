73995fbc81149a476a9b02fd9472e53e1f45ff95
Elizabeth Méndez <elixa_mendex@hotmail.com>
2025-10-02 17:24:52 -0600

    Add role model

---
+++ PosSystem.Model/Model/Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PosSystem.Model.Model
+{
+    internal class Role
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdRole {  get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public DateTime CreationDate { get; private set; }
+    }
+}
